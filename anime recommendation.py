0fb425b9419e6ec388d26a1703e2706f0e71b399
Kapilar12 <110399965+Kapilar12@users.noreply.github.com>
2023-09-21 11:47:09 +0530

    Add files via upload
    I provided the datasets and  code for anime recommendation service.Make sure you download the datasets and modify the dataset path in the code  where the csv file is located.

---
+++ anime recommendation.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+animes_df = pd.read_csv('E://anime.csv')
+ratings_df = pd.read_csv('E://ratings.csv')
+df = animes_df.merge(ratings_df, on='anime_id')
+anime=input()
+rcmd_animes = []
+animedatabase = df[df['name'] == anime]\
+            .sort_values(by='rating', ascending=False)
+for user in animedatabase.iloc[:5]['u_id'].values:
+    ranimes = df[df['u_id'] == user]
+    ranimes = ranimes[ranimes['name'] != anime]\
+                    .sort_values(by='rating', ascending=False)\
+                    .iloc[:5]
+    rcmd_animes.extend(list(ranimes['name'].values))
+    
+rcmd_animes = np.unique(rcmd_animes)
+    
+for i in rcmd_animes:
+    print(i)
+anime_genres_split = df[df['name'] == anime].iloc[0]['genre'].split('|')
+scores = {}
+
+for i in rcmd_animes:
+    anim = df[df['name'] == i].iloc[0]
+    anime_genres = anim['genre'].split('|')
+    score = 0
+    for k in anime_genres_split:
+        if k in anime_genres:
+            score += 1  
+    scores[i] = score
+rcmd_animes = sorted(scores, key=lambda x: scores[x])[::-1]
+for animee in rcmd_animes:
+    print(animee)
